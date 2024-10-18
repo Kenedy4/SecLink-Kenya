75998ba6030d55612a860749cd03289037354f5c
kenedy4 <kennedymusyoki8@gmail.com>
2024-10-18 12:33:45 +0300

    changed the model Class to have the get method only

--- seclinkkenya/server/routes/class.py
+++ seclinkkenya/server/routes/class.py
@@ -1,34 +1,20 @@
+from flask import request, jsonify
+from flask_restful import Resource
+from models import Class,  db
+# from seclinkkenya.server.routes.auth import token_required   # type: ignore
+from seclinkkenya.server.app import login_required
+
 class Class(Resource):
+    # @token_required 
     @login_required
     def get(self, class_id=None):
         if class_id:
-            class_obj = Class.query.get_or_404(class_id)
-            return class_obj.to_dict(), 200
-        classes = Class.query.all()
-        return [class_obj.to_dict() for class_obj in classes], 200
-
-    @login_required
-    def post(self):
-        data = request.get_json()
-        class_obj = Class(
-            class_name=data.get('class_name'),
-            teacher_id=data.get('teacher_id'),
-        )
-        db.session.add(class_obj)
-        db.session.commit()
-        return class_obj.to_dict(), 201
-
-    @login_required
-    def put(self, class_id):
-        data = request.get_json()
-        class_obj = Class.query.get_or_404(class_id)
-        class_obj.class_name = data.get('class_name')
-        db.session.commit()
-        return class_obj.to_dict(), 200
-
-    @login_required
-    def delete(self, class_id):
-        class_obj = Class.query.get_or_404(class_id)
-        db.session.delete(class_obj)
-        db.session.commit()
-        return '', 204
+            # Fetch the class by class_id
+            class_obj = Class.query.get(class_id)
+            if not class_obj:
+                return jsonify({'message': 'Class not found.'}), 404
+            return jsonify(class_obj.to_dict()), 200
+        else:
+            # Fetch all classes if no class_id is provided
+            classes = Class.query.all()
+            return jsonify([class_obj.to_dict() for class_obj in classes]), 200
